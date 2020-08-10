c5b04e82e72d2f873fdaa3abc95af132936c9705
Tung Duong <duongvutung1998@gmail.com>
2020-08-10 13:56:37 +0700

    add prop to seat dto

--- src/seat-checkin/dto/app.dto.ts
+++ src/seat-checkin/dto/app.dto.ts
@@ -2,6 +2,8 @@ export class SeatDto {
 
     id: string;
     delegateCode: string;
+    row: number;
+    column: number;
 
     setId(id: string): SeatDto{
         this.id = id;
@@ -10,6 +12,14 @@ export class SeatDto {
 
     setDelegateCode(code: string): SeatDto {
         this.delegateCode = code;
+        return this;
+    }
+    setRow(row: number): SeatDto {
+        this.row = row;
+        return this;
+    }
+    setColumn(column: number): SeatDto {
+        this.column = column;
         return this;
     }
 }
